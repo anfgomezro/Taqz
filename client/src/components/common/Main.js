a13dbc2fe2e64dac9f420f07eecee06e42366bcb
clarke32 <anfgomezro@nal.edu.co>
2018-03-30 21:07:19 -0500

    progress bar added, check back-end

--- client/src/components/common/Main.js
+++ client/src/components/common/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
-import {Home , Login, About} from './'
+import {Home , Login, About, Response} from './'
 
 function Main(props){
         return(
@@ -8,6 +8,7 @@ function Main(props){
                 <Route exact path='/' component={Home}/>
                 <Route path='/login' component={Login}/>
                 <Route path='/about' component={About}/>
+                <Route path='/api/hello' component={Response}/>
             </Switch>
         )
 }
